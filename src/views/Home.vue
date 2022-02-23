12f4b4fa5e2cea8b8fc2b80be105065bc6b0b3ab
rubenbch76 <rubenbch@gmail.com>
2022-02-23 09:39:43 +0100

    vuetify installed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="home">
-    <Notas />
-  </div>
+  <hello-world />
 </template>
 
 <script>
-// @ is an alias to /src
-import Notas from "@/components/Notas.vue";
+import HelloWorld from "../components/HelloWorld";
 
 export default {
   name: "Home",
+
   components: {
-    Notas,
+    HelloWorld,
   },
 };
 </script>
